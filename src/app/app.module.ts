8b5d07630a01c6d92783b204b966359b0ed84cea
mariabpulgar1991 <mariabernarda.pulgar474@comunidadunir.net>
2024-06-24 20:12:08 -0400

    carpeta assets anadida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerSectionComponent } from './componentes/banner-section/banner-section.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarAppComponent } from './componentes/navbar-app/navbar-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerSectionComponent,
+    HomeComponent,
+    NavbarAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/banner-section/banner-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-section',
+  templateUrl: './banner-section.component.html',
+  styleUrls: ['./banner-section.component.css']
+})
+export class BannerSectionComponent {
+  bannerQuote = `Que hermosa eres imperial Cartagena,
+            Coronada de murallas y antiguos balcones,
+            Con tu majestuosidad y señorío,
+            Que a los turistas robas sus corazones.`
+}
