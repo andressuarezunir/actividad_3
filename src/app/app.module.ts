c2480befaf43d9ec53f9c67ed346f5faa05f3d85
mariabpulgar1991 <mariabernarda.pulgar474@comunidadunir.net>
2024-06-24 20:47:45 -0400

    app maquetada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { BannerSectionComponent } from './componentes/banner-section/banner-section.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NavbarAppComponent } from './componentes/navbar-app/navbar-app.component';
+import { BlogSectionComponent } from './componentes/blog-section/blog-section.component';
+import { FeaturesSectionComponent } from './componentes/features-section/features-section.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { HeroSectionComponent } from './componentes/hero-section/hero-section.component';
+import { TestimonialComponent } from './componentes/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerSectionComponent,
     HomeComponent,
-    NavbarAppComponent
+    NavbarAppComponent,
+    BlogSectionComponent,
+    FeaturesSectionComponent,
+    FormularioComponent,
+    HeroSectionComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
