fa98d1a9ceddc920ca224ab33157038aa37ee3c5
mariabpulgar1991 <mariabernarda.pulgar474@comunidadunir.net>
2024-06-24 22:01:21 -0400

    changes in blog section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
+
 
 const routes: Routes = [
   { 
     path: '', 
     component: HomeComponent
   },
-  // {
-  //   path: 'contactanos', 
-  //   component: FormularioComponent
-  // }
+  {
+    path: 'contactanos', 
+    component: FormularioComponent
+  }
 ];
 
 @NgModule({
