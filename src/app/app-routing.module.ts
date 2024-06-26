f239cd08a5f97e0d135395ba7147f95680c3f55a
Andrés Suárez <andresfelipe.suarez154@comunidadunir.net>
2024-06-25 19:54:49 -0500

    Minor fix in styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
 
 
